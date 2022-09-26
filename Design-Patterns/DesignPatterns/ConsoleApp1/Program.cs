771a901a6d93303d205569d5d7f1bc30ccfafa75
Mohammad Atikur Rhaman <atikur.rhaman@enosisbd.com>
2022-09-26 21:24:58 +0600

    Start a solution to implement all the design pattern -Learn the basics of Singleton design pattern

---
+++ Design-Patterns/DesignPatterns/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SingletonDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton teacher = Singleton.GetInstance;
+            teacher.PrintDetails("From teacher object");
+
+            Singleton student = Singleton.GetInstance;
+            student.PrintDetails("From student object");
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ Design-Patterns/DesignPatterns/ConsoleApp1/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDemo
+{
+    internal sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+        public static Singleton GetInstance
+        {
+            get
+            {   
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter value : " + counter.ToString());
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
